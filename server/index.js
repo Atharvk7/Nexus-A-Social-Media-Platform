eec397418fe7e808100ba221085ac0b45fa4d0e6
Atharv Kulkarni <87613414+Atharvk7@users.noreply.github.com>
2023-02-01 22:06:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,16 +13,10 @@ app.use(cors())
 
 app.use('/posts',postRoutes);
 app.use('/user', userRoutes)
-//app.use('/posts',(req,res)=>{
- //   res.send('Hello')
-//})
 
-
-const Connection_url="mongodb://Atharv:Atharvk7@ac-q2ugnmd-shard-00-00.nolkfur.mongodb.net:27017,ac-q2ugnmd-shard-00-01.nolkfur.mongodb.net:27017,ac-q2ugnmd-shard-00-02.nolkfur.mongodb.net:27017/?ssl=true&replicaSet=atlas-pigdk9-shard-0&authSource=admin&retryWrites=true&w=majority"
+const Connection_url="Enter Your URL"
 const PORT = process.env.PORT || 5000;
-//app.listen(PORT,()=>console.log("server is running in a sexy way"))
 mongoose.connect(Connection_url,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>app.listen(PORT, ()=>console.log("Server is running on port 5000, Database is connected successfully")))
 .catch((error)=>console.log(error.message));
-//mongoose.set('useFindAndModify',false)
 
